13995a72ca3551570eadf65f75a136c76c962966
abdalrahman obaid <aboudaboudiua2@gmail.com>
2026-05-27 09:32:04 +0300

    minor validation fixes

---
+++ Rokhsetak/wwwroot/js/shared.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("click", function (e) {
+    const btn = e.target.closest("[data-alert-dismiss]");
+    if (!btn) return;
+
+    const alert = btn.closest(".alert");
+    if (alert) {
+        alert.remove();
+    }
+});
